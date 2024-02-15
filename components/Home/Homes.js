3d1aaad7472513786e990ab20fd2b78088283162
souvikinfluxiq <souvik@influxiqtech.com>
2024-02-15 16:47:14 +0530

    polaris home page img html

--- components/Home/Homes.js
+++ components/Home/Homes.js
@@ -6,7 +6,21 @@ export default function Homes() {
     
   return (
     <>
-            <p className={styles.inputs}>souvik</p>
+          <div className={styles.polaris_demo_wrp}>
+            <img src='https://all-frontend-assets.s3.amazonaws.com/polaris_project/home/home_bnr_demo.webp'/>
+          </div>
+          <div className={styles.polaris_demo_wrp}>
+            <img src='https://all-frontend-assets.s3.amazonaws.com/polaris_project/home/polaris_home_blk1_img.webp'/>
+          </div>
+          <div className={styles.polaris_demo_wrp}>
+            <img src='https://all-frontend-assets.s3.amazonaws.com/polaris_project/home/polaris_home_blk2_img.webp'/>
+          </div>
+          <div className={styles.polaris_demo_wrp}>
+            <img src='https://all-frontend-assets.s3.amazonaws.com/polaris_project/home/polaris_home_blk3_img.webp'/>
+          </div>
+          <div className={styles.polaris_demo_wrp}>
+            <img src='https://all-frontend-assets.s3.amazonaws.com/polaris_project/home/polaris_home_last_blk_img.webp'/>
+          </div>
     </>
   )
 }
