19f48e349bf12d0c3ff16a59785623109ffa5c46
souvikinfluxiq <souvik@influxiqtech.com>
2024-02-15 16:18:01 +0530

    polaris global css m,odifications

--- components/Home/Homes.js
+++ components/Home/Homes.js
@@ -2,6 +2,8 @@ import React from 'react'
 import styles from './home.module.css'
 
 export default function Homes() {
+
+    
   return (
     <>
             <p className={styles.inputs}>souvik</p>
