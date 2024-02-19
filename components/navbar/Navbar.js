bb831b9402da841856d45c2e92ccbb4c4ebbab55
raajdepp <rajdeep@influxiqtech.com>
2024-02-19 12:03:06 +0530

    for pull

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -1,3 +1,5 @@
+
+import { Link } from '@mui/material'
 import React from 'react'
 import styles from './navbar.module.css'
 
@@ -9,7 +11,7 @@ export default function Navbar() {
       <div className={styles.common_width}>
       <div className={styles.polaris_header_sub1_wrp}>
         <div className={styles.polaris_header_sub1_sec1_wrp}>
-          <img src='https://all-frontend-assets.s3.amazonaws.com/polaris_project/home/poralis_header_logo.png'/>
+         <Link> <img src='https://all-frontend-assets.s3.amazonaws.com/polaris_project/home/poralis_header_logo.png'/></Link>
         </div>
 
         <div className={styles.polaris_header_sub1_sec2_wrp}>
@@ -31,11 +33,11 @@ export default function Navbar() {
       <div className={styles.polaris_header_sub2_wrp}>
         <div className={styles.polaris_header_sub2_left_wrp}>
           <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Services</li>
-            <li>Blog</li>
-            <li>Contact</li>
+            <li><Link>Home</Link></li>
+            <li><Link>About</Link></li>
+            <li><Link>Services</Link></li>
+            <li><Link>Blog</Link></li>
+            <li><Link>Contact</Link></li>
 
 
 
