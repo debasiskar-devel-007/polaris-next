b55e8b3bdea3df67f60ab75bb6c4bc18b9c25138
Soubhik Debnath <soubhik@influxiqtech.com>
2024-02-20 17:06:07 +0530

    polaris business consultants  block 1 add contant and html or css  development [[bill-01:00-6]]

--- pages/polaris-business-consultants/index.js
+++ pages/polaris-business-consultants/index.js
@@ -41,7 +41,75 @@ export default function Index() {
                <p>Welcome to the exciting world of Ambulatory Surgery Center Development! This comprehensive course is designed to be your guide through the intricacies of establishing and developing Ambulatory Surgery Centers. Whether you’re a healthcare professional, administrator, or entrepreneur looking to develop in the dynamic field of outpatient surgical care, this course is tailored to equip you with the Knowledge, tools, and skills needed for successful Ambulatory Surgical Center Development.</p>
                <p>Over  the duration of this course, we will explore the key components of Ambulatory Surgical Center Development, from regulatory considerations and compliance to operational efficiency and strategic planning. Gain insight into the latest industry trends, best practices, and essential tools that will empower you to navigate the complex landscape of Ambulatory Surgical Center Development with confidence.</p>
                <p>Our goal is to provide you with a comprehensive understanding of the various facets involved in establishing and managing ASCs, enabling you to contribute to the delivery of high-quality, patient-centered care in the outpatient setting. Get ready to embark on a transformative learning journey that will position you at the forefront of Ambulatory Surgical Center Development. Let’s dive in!</p>
-               
+
+            </div>
+         </div>
+      </div>
+      <div className={styles.PBC_block2}>
+         <div className={styles.common_width}>
+            <div className={styles.PBC_block2_wrpr}>
+               <h1>AMBULATORY SURGERY CENTER DEVELOPMENT COURSE OUTLINE</h1>
+               <p>This is a six-week course designed to give the participant an understanding of the stages of development when developing a new ambulatory surgery center. The course will include the following sessions:</p>
+               <div className={`${styles.PBC_boxes} ${styles.windowBulletOrange}`}>
+                  <div className={styles.PBC_InnerBox}>
+                     <h2>Module 1: Project Assessment</h2>
+                     <ul>
+                        <li>Introduction</li>
+                        <li>Market Study</li>
+                        <li>Strategic Planning</li>
+                     </ul>
+                  </div>
+                  <div className={styles.PBC_InnerBox}>
+                     <h2>Module 2: Pre-Project Planning</h2>
+                     <ul>
+                        <li>Preliminary Design </li>
+                        <li>Investment Structure </li>
+                        <li>Financial Projection - Initial</li>
+                        <li>Real Estate</li>
+                     </ul>
+                  </div>
+                  <div className={styles.PBC_InnerBox}>
+                     <h2>Module 3: Planning and Development: Facility Design and Construction</h2>
+                     <ul>
+                        <li>Legal / Regulatory / Organizational</li>
+                        <li>Financial Projections</li>
+                        <li>Facility Development: Design and Construction</li>
+                     </ul>
+                  </div>
+                  <div className={styles.PBC_InnerBox}>
+                     <h2>Module 4: Planning and Development: Non-Clinical Operations</h2>
+                     <ul>
+                        <li>Administration</li>
+                        <li>Human Resources</li>
+                        <li>Medical Records</li>
+                        <li>Facility</li>
+                        <li>Financial Management</li>
+                        <li>Marketing</li>
+                        <li>Support Services</li>
+                        <li>Miscellaneous</li>
+                     </ul>
+                  </div>
+                  <div className={styles.PBC_InnerBox}>
+                     <h2>Module 5: Planning and Development: Clinical Operations</h2>
+                     <ul>
+                        <li>Administration</li>
+                        <li>Supporting Physician Coverage Models</li>
+                        <li>Policies and Procedures</li>
+                        <li>Medical Staff</li>
+                        <li>Ancillary Services </li>
+                        <li>Quality Preparedness</li>
+                     </ul>
+                  </div>
+                  <div className={styles.PBC_InnerBox}>
+                     <h2>Module 6: Go Live</h2>
+                     <ul>
+                        <li>Grand Opening</li>
+                        <li>Test Clinical Systems</li>
+                        <li>Test Non-Clinical Systems</li>
+                        <li>Accreditation Preparedness</li>
+                     </ul>
+                  </div>
+               </div>
             </div>
          </div>
       </div>
