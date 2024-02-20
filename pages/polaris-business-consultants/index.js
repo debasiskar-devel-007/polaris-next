a3747d6618badda52f3a7f41a1e69bede1731e58
Soubhik Debnath <soubhik@influxiqtech.com>
2024-02-20 15:30:38 +0530

    polaris business consultants  block 1 add contant and html or css  development [[bill-01:00-6]]

--- pages/polaris-business-consultants/index.js
+++ pages/polaris-business-consultants/index.js
@@ -29,7 +29,18 @@ export default function Index() {
      {/* <div className={styles.demo_1img}>
         <img src='https://all-frontend-assets.s3.amazonaws.com/polaris_project/business_banner_img.webp'/>
      </div> */}
-     <div className={styles.demo_img}>
+      <div className={styles.PBC_block1}>
+         <div className={styles.common_width}>
+            <div className={styles.PBC_block1_wrpr}>
+               <h1>AMBULATORY SURGERY CENTER DEVELOPMENT COURSE</h1>
+               <p>Welcome to the exciting world of Ambulatory Surgery Center Development! This comprehensive course is designed to be your guide through the intricacies of establishing and developing Ambulatory Surgery Centers. Whether you’re a healthcare professional, administrator, or entrepreneur looking to develop in the dynamic field of outpatient surgical care, this course is tailored to equip you with the Knowledge, tools, and skills needed for successful Ambulatory Surgical Center Development.</p>
+               <p>Over  the duration of this course, we will explore the key components of Ambulatory Surgical Center Development, from regulatory considerations and compliance to operational efficiency and strategic planning. Gain insight into the latest industry trends, best practices, and essential tools that will empower you to navigate the complex landscape of Ambulatory Surgical Center Development with confidence.</p>
+               <p>Our goal is to provide you with a comprehensive understanding of the various facets involved in establishing and managing ASCs, enabling you to contribute to the delivery of high-quality, patient-centered care in the outpatient setting. Get ready to embark on a transformative learning journey that will position you at the forefront of Ambulatory Surgical Center Development. Let’s dive in!</p>
+               
+            </div>
+         </div>
+      </div>
+     {/* <div className={styles.demo_img}>
         <img src='https://all-frontend-assets.s3.amazonaws.com/polaris_project/business_blk1_img.webp'/>
      </div>
      <div className={styles.demo_img}>
@@ -40,7 +51,7 @@ export default function Index() {
      </div>
      <div className={styles.demo_img}>
         <img src='https://all-frontend-assets.s3.amazonaws.com/polaris_project/business_blk4_img.webp'/>
-     </div>
+     </div> */}
     <Footer />
     </>
    
