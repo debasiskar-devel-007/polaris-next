b1dd1f49361db05a5e13f55b42837bbfdb53613e
Soubhik Debnath <soubhik@influxiqtech.com>
2024-02-20 12:51:01 +0530

    polaris-business-consultants banner  html development [[bill-01:00-6]]

--- pages/polaris-business-consultants/index.js
+++ pages/polaris-business-consultants/index.js
@@ -2,12 +2,31 @@ import React from 'react'
 import styles from './businessconsulyants.module.css'
 import Footer from '@/components/Footer'
 import Navbar from '@/components/navbar/Navbar'
+import Head from 'next/head'
 
 export default function Index() {
   return (
     <>
+    <Head>
+      <title>Polaris Business Consultants</title>
+    </Head>
+
      <Navbar />
-     <div className={styles.demo_1img}>
+     <div className={styles.PBC_banner}>
+      <div className={styles.common_width}>
+         <div className={styles.PBC_banner_wrpr}>
+            <div className={styles.PBC_bannerTop}>
+               {/* <img src="https://all-frontend-assets.s3.amazonaws.com/polaris_project/polaris-business-consultants/docGlobe.png" /> */}
+               <h1>Polaris Business Consultants</h1>
+            </div>
+            <div className={styles.PBC_bannerBottom}>
+               <p>Ambulatory Surgical Center Development Course</p>
+               <img src="https://all-frontend-assets.s3.amazonaws.com/polaris_project/polaris-business-consultants/PBC_bannerBottomLogo.png" />
+            </div>
+         </div>
+      </div>
+     </div>
+     {/* <div className={styles.demo_1img}>
         <img src='https://all-frontend-assets.s3.amazonaws.com/polaris_project/business_banner_img.webp'/>
      </div>
      <div className={styles.demo_img}>
@@ -21,7 +40,7 @@ export default function Index() {
      </div>
      <div className={styles.demo_img}>
         <img src='https://all-frontend-assets.s3.amazonaws.com/polaris_project/business_blk4_img.webp'/>
-     </div>
+     </div> */}
     <Footer />
     </>
    
