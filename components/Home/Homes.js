3e769979e1a9fafe70de4ae5d83100654a7f23c1
souvikinfluxiq <souvik@influxiqtech.com>
2024-02-20 11:09:26 +0530

    home page

--- components/Home/Homes.js
+++ components/Home/Homes.js
@@ -121,9 +121,9 @@ export default function Homes() {
     if (!user?.address) {
       errorObject.address = "Address is Required";
     }
-    if (!user?.write_a_message) {
-      errorObject.write_a_message = "Write a Message is Required";
-    }
+    // if (!user?.write_a_message) {
+    //   errorObject.write_a_message = "Write a Message is Required";
+    // }
 
 
     if (Object.keys(errorObject).length > 0) {
