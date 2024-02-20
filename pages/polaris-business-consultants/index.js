1b7dbb74792586d6c5ed22a4dd4bf478daf9422b
Soubhik Debnath <soubhik@influxiqtech.com>
2024-02-20 13:36:49 +0530

    polaris-business-consultants banner  css  development [[bill-01:00-6]]

--- pages/polaris-business-consultants/index.js
+++ pages/polaris-business-consultants/index.js
@@ -28,7 +28,7 @@ export default function Index() {
      </div>
      {/* <div className={styles.demo_1img}>
         <img src='https://all-frontend-assets.s3.amazonaws.com/polaris_project/business_banner_img.webp'/>
-     </div>
+     </div> */}
      <div className={styles.demo_img}>
         <img src='https://all-frontend-assets.s3.amazonaws.com/polaris_project/business_blk1_img.webp'/>
      </div>
@@ -40,7 +40,7 @@ export default function Index() {
      </div>
      <div className={styles.demo_img}>
         <img src='https://all-frontend-assets.s3.amazonaws.com/polaris_project/business_blk4_img.webp'/>
-     </div> */}
+     </div>
     <Footer />
     </>
    
