865758f0a33a7fdf5d59da4975baad5e503cf0b8
Biswarup2000 <biswarup@influxiqtech.com>
2024-02-21 15:41:23 +0530

    POLARIS HTML BIO Jay Boughner inner page banner section HTML CSS in done[[bill-01:00-13-02212024]]

--- pages/bio-jayboughner/index.js
+++ pages/bio-jayboughner/index.js
@@ -6,19 +6,39 @@ import styles from './jayB.module.css'
 export default function Index() {
   return (
     <>
-    <Navbar />
-      <div className={styles.polaris_JB_BIO_width}>
-   
+
+  <div className="polaris_j_boughner_page_main_width">
+
+    <div className={styles.polaris_j_boughner_page_full_width}>
+
+      <div className={styles.j_boughner_page_banner_main_width}>
+        <Navbar/>
+          <div className={styles.common_width}>
+            <div className={styles.j_boughner_bnr_left_cont}>
+              <h1>BIO</h1>
+  
+              <div className={styles.yellow_box_sec_main_width}>
+                <h2>Our leadership team</h2>
+              </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+
+    {/* <div className={styles.polaris_JB_BIO_width}>
       <div className={styles.jb_bio_page}>
         <img src="https://all-frontend-assets.s3.amazonaws.com/polaris_project/home/polaris_jay_bughner_bio_bnnr_img.webp"/>
       </div>
       <div className={styles.jb_bio_page}>
         <img src="https://all-frontend-assets.s3.amazonaws.com/polaris_project/home/polaris_jay_bughner_bio_block1_img.webp"/>
       </div>
-      
-      
-    </div>
+    </div> */}
+    
     <Footer />
+
+  </div>
     </>
   )
 }
