5f3dea0b275b47f4c1a1b734b75427823d4b07d2
Biswarup2000 <biswarup@influxiqtech.com>
2024-02-22 13:33:32 +0530

    POLARIS HTML Home inner page BIO John D Martin page block1 section full HTML CSS done[[bill-01:00-13-02222024]]

--- pages/bio-johnmartin/index.js
+++ pages/bio-johnmartin/index.js
@@ -40,6 +40,34 @@ export default function index() {
         </div>
       </div>
 
+      <div className={styles.j_martin_page_block1_main_width}>
+        <div className={styles.common_width}>
+          <div className={styles.j_martin_page_block1_white_box}>
+            <div className={styles.block1_white_box_grey_bar}>
+              <h1>John D Martin</h1>
+            </div>
+
+            <div className={styles.block1_white_box_main_wrpr}>
+
+              <div className={styles.block1_white_box_left_sec}>
+                <div className={styles.white_box_left_sec_img_box}>
+                  <div className={styles.left_sec_img_sub_box}>
+                    <img src='https://all-frontend-assets.s3.amazonaws.com/polaris_project/home/john_d_martin/john_d_martin_img.webp'></img>
+                  </div>
+                </div>
+              </div>
+
+              <div className={styles.block1_white_box_right_sec}>
+                <p>John Martin is the Managing Director – Healthcare Consulting of Polaris Business Consultants. With over thirty-five years of experience in healthcare, he has worked in large medical practices, specialty hospitals, and health systems. John graduated with an accounting degree from the Indiana University Kelley School of Business in 1988. His career has included roles as Director of Business Development, Administrator, Chief Financial Officer, and Chief Executive Officer.</p>
+
+                <p>John began his consulting career in 2012 after leaving his role as Chief Executive Officer of Orthopaedics Indianapolis in Indianapolis, IN. Throughout his career in healthcare, he has participated in more than 20 development projects including hospitals, specialty hospitals, ambulatory surgery centers, and medical office buildings. The cornerstone of every project has been to achieve alignment between hospitals and physicians in the delivery of healthcare services. He has participated in service line development covering every major specialty in healthcare delivery improving financial performance, care delivery, and creating specialty specific physician alignment strategies.</p>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
 
   </div>
